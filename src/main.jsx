ae93c755f2f7851bf2b54ee7ad913c5f484fa99f
adityajha1023 <adityajha.1023@gmail.com>
2026-01-24 22:02:01 +0530

    new

--- src/main.jsx
+++ src/main.jsx
@@ -1,35 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-import { MemoryRouter, Routes, Route } from 'react-router-dom'
+import Layout from "./Layout";
+import App from "./App";
+import Project1 from "./project1";
+import NotFound from "./components/NotFound";
 
-import App from './App.jsx'
-import Layout from './Layout.jsx'
-import Project from './project1.jsx'
-import NotFound from './components/NotFound.jsx'
+import "./index.css";
 
-import Lenis from 'lenis'
-
-// Initialize Lenis
-const lenis = new Lenis({
-  autoRaf: true,
-})
-
-lenis.on('scroll', (e) => {
-  console.log(e)
-})
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <MemoryRouter>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<App />} />
-          <Route path="project1" element={<Project />} />
-          <Route path="*" element={<NotFound />} />
+        {/* Layout wrapper */}
+        <Route element={<Layout />}>
+          
+          {/* Home */}
+          <Route path="/" element={<App />} />
+
+          {/* Project Page */}
+          <Route path="/project1" element={<Project1 />} />
+
         </Route>
+
+        {/* Fallback */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
-    </MemoryRouter>
-  </StrictMode>
-)
+    </BrowserRouter>
+  </React.StrictMode>
+);
