da0ef9186152c004093d49bb27770d9a0438b129
adityajha1023 <adityajha.1023@gmail.com>
2026-01-24 21:50:20 +0530

    adsfas

--- src/main.jsx
+++ src/main.jsx
@@ -1,47 +1,35 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+
+import { MemoryRouter, Routes, Route } from 'react-router-dom'
+
 import App from './App.jsx'
-import ReactDOM from "react-dom/client";
-import {createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Layout from './Layout.jsx';
-import Project from './project1.jsx';
-import NotFound from './components/NotFound.jsx';
+import Layout from './Layout.jsx'
+import Project from './project1.jsx'
+import NotFound from './components/NotFound.jsx'
+
 import Lenis from 'lenis'
 
 // Initialize Lenis
 const lenis = new Lenis({
   autoRaf: true,
-});
+})
 
-// Listen for the scroll event and log the event data
 lenis.on('scroll', (e) => {
-  console.log(e);
-});
-
-const router=createBrowserRouter([
-  {
-    path:'/',
-    element:<Layout />,
-    children:[
-      {
-        path: '/',
-        element:  <App />
-      },
-      {
-        path: '/project1',
-        element: <Project  />
-      },
-      {
-        path: '*',
-        element: <NotFound />
-      }
-    ]
-  }
-])
+  console.log(e)
+})
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <RouterProvider router={router}/>
-  </StrictMode>,
+    <MemoryRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<App />} />
+          <Route path="project1" element={<Project />} />
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </MemoryRouter>
+  </StrictMode>
 )
