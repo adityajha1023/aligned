5072872c6d9e793a35614486a5a2db56ad5027f4
adityajha1023 <adityajha.1023@gmail.com>
2026-01-24 22:08:49 +0530

    adsf

--- src/Layout.jsx
+++ src/Layout.jsx
@@ -3,12 +3,10 @@ import Header from './components/Header'
 import App from './App'
 import Footer from './components/Footer'
 import { Outlet } from 'react-router'
-import ScrollToTop from './components/ScrollToTop'
 
 function Layout() {
     return (
         <div>
-            <ScrollToTop />
             <Header />
             <Outlet />
             <Footer />
